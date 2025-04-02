9ef695e9206a4094eb6dd5ae2b247348b4f8b8da
Equirizon <equirizon@gmail.com>
2025-04-03 05:19:36 +0800

    pass testing

--- script.js
+++ script.js
@@ -2,65 +2,85 @@ import buttonShine from './modules/button_shine_module.mjs';
 import myLogo from './modules/logo_module.mjs';
 
 
-const hello = document.querySelectorAll('.hello div');
 const button = document.getElementById('search-button');
 const input = document.getElementById('search-input');
 const mainStats = document.querySelectorAll('.stats');
 const typesElement = document.getElementById('types');
-
+const spriteElement = document.querySelector('.sprite');
+const spriteContainer = document.querySelector('.sprite-container');
 
 myLogo('display', '6.5rem');
 
 const loading = () => {
-    typesElement.innerHTML = 'loading...'
+    typesElement.innerHTML = 'Loading...';
     mainStats.forEach((_, index) => {
-        mainStats[index].textContent = 'loading...';
+        mainStats[index].textContent = 'Loading...';
     });
-}
+    spriteElement.src = '/assets/pokeball.svg';
+    spriteElement.classList.add('rotate');
+};
 
+const notFound = () => {
+    typesElement.innerHTML = '';
+    mainStats.forEach((_, index) => {
+        mainStats[index].textContent = 'Pokémon not found!';
+    });
+    spriteElement.classList.remove('rotate');
+    spriteElement.src = '/assets/pokeball-error.svg';
+};
+ 
 const parseData = (pokemon) => {
     const { name, id, weight, height } = pokemon;
     const sprite = pokemon.sprites.front_default;
+
     for (let index = 0; index < 4; index++) {
-        mainStats[index].textContent = `${[name, id, weight, height][index]}`;
+        mainStats[index].textContent = `${[name.toUpperCase(), '#' + id, weight, height][index]}`;
     }
 
     pokemon.stats.forEach((stat, index) => {
         mainStats[index + 4].textContent = stat.base_stat;
-    })
+    });
     
-    typesElement.innerHTML = ''
+    typesElement.innerHTML = '';
     pokemon.types.forEach((types) => {
         const pokemonType = types.type.name;
-        typesElement.innerHTML += `<span class="${pokemonType}-type">${pokemonType.toUpperCase()}</span>`
-    })
+        typesElement.innerHTML += `<span class="${pokemonType}-type">${pokemonType.toUpperCase()}</span>`;
+    });
 
-    document.querySelector('.sprite-container').src = sprite;
-}
+    spriteElement.src = sprite;
+    spriteElement.classList.remove('rotate');
+};
 
 const fetchData = async (pokemon) => {
     try {
         const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemon}`);
-        const data = await response.json();
-        parseData(data)
+        let data;
+        if (response.ok) {
+            data = await response.json();
+        } else {
+            throw new Error(`Status: ${response.status}`);
+        }
+        parseData(data);
     } catch (error) {
-        console.error(error);
-        alert("Pokemon doesn't exist");
-        //TODO: add fallback
+        console.error('There is a problem resolving the API:', error);
+        alert("Pokémon not found");
+        notFound();
     };
-}
+};
 
-button.addEventListener('click', () => {
+button.addEventListener('click', (e) => {
+    e.preventDefault();
     loading();
-    fetchData(input.value);
+    fetchData(input.value.toLowerCase());
 });
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
+        e.preventDefault();
         loading();
-        fetchData(input.value);
+        fetchData(input.value.toLowerCase());
     }
-})
+});
 
 
 
